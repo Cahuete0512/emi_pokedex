dc50cff67407df8af92d51e7c3ba09cd28b4539c
Cahuete0512 <74617561+Cahuete0512@users.noreply.github.com>
2022-11-25 19:51:48 +0100

    Added Statics folder for images, style and javascript + Essays Search function to be reviewed + styling the app

--- EMI_Poke_app/EMI_Poke/settings.py
+++ EMI_Poke_app/EMI_Poke/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -115,8 +115,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
+STATIC_URL = '/static/'
 
-STATIC_URL = 'static/'
+# this is the static files folder name which you created in django project root folder.
+# This is different from above STATIC_URL.
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
